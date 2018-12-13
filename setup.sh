be4dd15cdd965ed33c36f083ebdf24f66ca802af
Fionn O'Connor <foconnor@decaresystems.ie>
2018-12-13 10:06:36 +0000

    git setup + more aliases

--- setup.sh
+++ setup.sh
@@ -2,8 +2,15 @@
 #
 # Set up config
 
-curDir=$(pwd)
+curDir=$(dirname $0)
 
-echo "source $(pwd)/aliases" >> ~/.bash_rc
-cp $( pwd )/ideavimrc $HOME/.ideavimrc
+echo "source $(curDir)/aliases" >> ~/.bash_rc
+cp $(curDir)/ideavimrc $HOME/.ideavimrc
 source ./install_vim_extended.sh
+
+
+# Git config
+git config --global core.editor "vim"
+git config --global alias.pull 'pull -r'
+git config --global alias.append  'commit --amend --no-edit'
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
