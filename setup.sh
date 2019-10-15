d6261d81fb98796360b722021f12892e6de33516
Fionn O'Connor <foconnor@decaresystems.ie>
2019-10-15 10:54:21 +0100

    updates

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ cd `dirname $0`
 DOTFILES=`pwd`
 
 echo "source $DOTFILES/aliases" >> ~/.bash_rc
+echo "source $DOTFILES/aliases" >> ~/.bash_profile
 cp $DOTFILES/ideavimrc $HOME/.ideavimrc
 source ./install_vim_extended.sh
 
@@ -17,3 +18,6 @@ git config --global core.editor "vim"
 git config --global alias.pull 'pull -r'
 git config --global alias.append  'commit --amend --no-edit'
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.last "log -1 --stat"
+git config --global alias.cp "cherry-pick"
+git config --global alias.co "checkout"
